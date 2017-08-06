7693035ad7d88573ee59d45941ddb108444ae058
Charmi <charmisharma577@gmail.com>
2017-08-06 16:50:29 +0530

    files uploaded

---
+++ hun.py
@@ -0,0 +1,15 @@
+
+
+ #int("Enter your name and age? ")
+#print("name : ")
+
+#name = input("name : ")
+#age  = int(input("age : "))
+
+name = "Charmi"
+age  = 21
+
+
+year_100 = int(2017 + (100 - age))
+
+print("%s! You will be 100 years old in %d" % (name, year_100))
